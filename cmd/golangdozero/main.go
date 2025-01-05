3ef85bde8e597308adff3908f086a02a239f2b9b
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2025-01-05 12:00:01 -0300

    feat: adds validate email  to main program

--- cmd/golangdozero/main.go
+++ cmd/golangdozero/main.go
@@ -1,5 +1,8 @@
 package main
 
+import "github.com/fabianoflorentino/golangdozero/pkg/sendmail"
+
 func main() {
 	println("Aprenda Golang do Zero!")
+	println(sendmail.CheckEmail("fabianoflorentino@outlook.com"))
 }
