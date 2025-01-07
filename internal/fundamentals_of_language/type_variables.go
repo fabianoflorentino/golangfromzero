fef384e25a2e1a3600fe45ff07b2ae93160e5cac
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2025-01-07 05:14:27 -0300

    fix: translate data to english and improve function documentation

--- internal/fundamentals_of_language/type_variables.go
+++ internal/fundamentals_of_language/type_variables.go
@@ -4,9 +4,9 @@ package fundamentals_of_language
 
 import "fmt"
 
-// Package fundamentos_da_linguagem provides fundamental examples and explanations
-// of the Go programming language, including variable declarations, constants,
-// and formatted string operations.
+// TypeVariables demonstrates the declaration and initialization of different types of variables
+// and constants in Go. It returns a formatted string that includes the values and types of these
+// variables and constants.
 func TypeVariables() string {
 	var var1 string = "variable 1"
 	var2 := "variable 2"
@@ -18,12 +18,12 @@ func TypeVariables() string {
 
 	var5, var6 := "variable 5", "variable 6"
 
-	const constante1 string = "Constante 1"
+	const constant1 string = "Constante 1"
 
-	variavel := fmt.Sprintf(
-		"\nvar1: %s :: %T\nvar2: %s :: %T\nvar3: %s :: %T\nvar4: %s :: %T\nvar5: %s :: %T\nvar6: %s :: %T\nconstante1: %s :: %T",
-		var1, var1, var2, var2, var3, var3, var4, var4, var5, var5, var6, var6, constante1, constante1,
+	variable := fmt.Sprintf(
+		"\nvar1: %s :: %T\nvar2: %s :: %T\nvar3: %s :: %T\nvar4: %s :: %T\nvar5: %s :: %T\nvar6: %s :: %T\nconstant1: %s :: %T",
+		var1, var1, var2, var2, var3, var3, var4, var4, var5, var5, var6, var6, constant1, constant1,
 	)
 
-	return variavel
+	return variable
 }
