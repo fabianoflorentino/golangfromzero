a2ca6b3ee57ae161cb12ed894590531eead5f20d
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2025-01-07 05:13:59 -0300

    fix: translate data to english

--- internal/fundamentals_of_language/type_of_data.go
+++ internal/fundamentals_of_language/type_of_data.go
@@ -5,7 +5,7 @@ package fundamentals_of_language
 
 import "fmt"
 
-// TiposDeDados demonstrates the declaration and usage of various data types in Go.
+// TypeOfData demonstrates the declaration and usage of various data types in Go.
 // It includes examples of integer types (int64, int32), unsigned integer types (uint64, uint32),
 // floating-point types (float64, float32), complex number types (complex64, complex128),
 // boolean type (bool), string type (string), and byte type (byte).
@@ -14,8 +14,8 @@ func TypeOfData() string {
 	var IntNumber64 int64 = 1000000000000000000
 	var IntNumber32 int32 = 1000000000
 
-	var numberUintero64 uint64 = 1000000000000000000
-	var numberUintero32 uint32 = 1000000000
+	var numberUinteger64 uint64 = 1000000000000000000
+	var numberUinteger32 uint32 = 1000000000
 
 	var numberReal64 float64 = 1000000000000000000.0
 	var numberReal32 float32 = 1000000000.0
@@ -23,16 +23,16 @@ func TypeOfData() string {
 	var numberComplex complex64 = 1 + 1i
 	var numberComplex2 complex128 = 1 + 1i
 
-	var booleano bool = true
+	var boolean bool = true
 
 	var texto string = "Texto"
 
-	var caracter byte = 'A'
+	var character byte = 'A'
 
-	variavel := fmt.Sprintf(
-		"\nIntNumber64: %d :: %T\nIntNumber32: %d :: %T\nnumeroUintero64: %d :: %T\nnumeroUintero32: %d :: %T\nnumeroReal64: %f :: %T\nnumeroReal32: %f :: %T\nnumeroComplexo: %f :: %T\nnumeroComplexo2: %f :: %T\nbooleano: %t :: %T\ntexto: %s :: %T\ncaracter: %c :: %T",
-		IntNumber64, IntNumber64, IntNumber32, IntNumber32, numberUintero64, numberUintero64, numberUintero32, numberUintero32, numberReal64, numberReal64, numberReal32, numberReal32, numberComplex, numberComplex, numberComplex2, numberComplex2, booleano, booleano, texto, texto, caracter, caracter,
+	variable := fmt.Sprintf(
+		"\nIntNumber64: %d :: %T\nIntNumber32: %d :: %T\nnumberUinteger64: %d :: %T\nnumberUinteger32: %d :: %T\nnumberReal64: %f :: %T\nnumberReal32: %f :: %T\nnumberComplex: %f :: %T\nnumberComplex2: %f :: %T\nboolean: %t :: %T\ntexto: %s :: %T\ncharacter: %c :: %T",
+		IntNumber64, IntNumber64, IntNumber32, IntNumber32, numberUinteger64, numberUinteger64, numberUinteger32, numberUinteger32, numberReal64, numberReal64, numberReal32, numberReal32, numberComplex, numberComplex, numberComplex2, numberComplex2, boolean, boolean, texto, texto, character, character,
 	)
 
-	return variavel
+	return variable
 }
