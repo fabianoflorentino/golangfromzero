f295b1efc3adfd76706c2fd890dbd1a9be5c8b3d
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2025-01-15 09:00:25 -0300

    feat: add documentation for fixed-length arrays in ArraysAndSlices example

--- internal/fundamentals_of_language/arrays_and_slices.go
+++ internal/fundamentals_of_language/arrays_and_slices.go
@@ -1,3 +1,5 @@
+// Package fundamentals_of_language provides examples and demonstrations of fundamental concepts in the Go programming language.
+// This package includes examples of creating and initializing fixed-length arrays, as well as other basic language features.
 package fundamentals_of_language
 
 import (
@@ -6,6 +8,11 @@ import (
 	"github.com/fabianoflorentino/golangfromzero/pkg/trim"
 )
 
+// exampleArrayWithLength contains a string literal that demonstrates the creation and initialization
+// of fixed-length arrays in Go. It includes three examples:
+// Example 1: Declares an array of 5 strings, assigns a value to the first element, and prints it.
+// Example 2: Declares and initializes an array of 5 strings with specific values.
+// Example 3: Declares and initializes an array of integers using the ellipsis syntax to infer the length.
 var (
 	exampleArrayWithLength = `
 // arrayWithLength demonstrates the creation and initialization of a fixed-length array in Go.
@@ -26,6 +33,10 @@ arr3 := [...]int{1, 2, 3, 4, 5}
 `
 )
 
+// ArraysAndSlices demonstrates the usage of fixed-length arrays in Go.
+// It initializes a new trim object, prints a message indicating the use of fixed-length arrays,
+// and then prints the trimmed string representation of an example array with a fixed length.
+// Finally, it calls the arrayWithLength function to further illustrate array operations.
 func ArraysAndSlices() {
 	trim := trim.New()
 
