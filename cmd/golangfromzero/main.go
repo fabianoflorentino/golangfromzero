d5c70d4a693e152a8b0ccb290625d68351701c04
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2025-03-08 12:47:16 -0300

    feat: update main function to use fmt.Println for output and add Maps section

--- cmd/golangfromzero/main.go
+++ cmd/golangfromzero/main.go
@@ -1,40 +1,45 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/fabianoflorentino/golangfromzero/internal/fundamentals_of_language"
 )
 
 func main() {
-	println("Leard Golang from Zero!")
+	fmt.Println("Leard Golang from Zero!")
 
-	println("\n5. External Packages\n")
-	println("fabianoflorentino@outlook.com: ", fundamentals_of_language.ExternalPackages("fabianoflorentino@outlook.com"))
-	println("invalid: ", fundamentals_of_language.ExternalPackages("invalid"))
+	fmt.Println("\n5. External Packages\n")
+	fmt.Println("fabianoflorentino@outlook.com: ", fundamentals_of_language.ExternalPackages("fabianoflorentino@outlook.com"))
+	fmt.Println("invalid: ", fundamentals_of_language.ExternalPackages("invalid"))
 
-	println("\n6. Variables")
-	println(fundamentals_of_language.TypeVariables())
+	fmt.Println("\n6. Variables")
+	fmt.Println(fundamentals_of_language.TypeVariables())
 
-	println("\n7. Type of Data")
-	println(fundamentals_of_language.TypeOfData())
+	fmt.Println("\n7. Type of Data")
+	fmt.Println(fundamentals_of_language.TypeOfData())
 
-	println("\n8. Type of Functions")
+	fmt.Println("\n8. Type of Functions")
 	fundamentals_of_language.TypeOfFunctions()
 
-	println("\n9. Operators")
+	fmt.Println("\n9. Operators")
 	fundamentals_of_language.Operators()
 
-	println("\n10. Type of Struct")
+	fmt.Println("\n10. Type of Struct")
 	fundamentals_of_language.TypeOfStruct()
 
-	println("\n11. Inheritance but not")
+	fmt.Println("\n11. Inheritance but not")
 	fundamentals_of_language.InheritanceButNot()
 
-	println("\n12. Arrays and Slices")
+	fmt.Println("\n12. Arrays and Slices")
 	fundamentals_of_language.ArraysAndSlices()
 
-	println("\n13. Pointers")
+	fmt.Println("\n13. Pointers")
 	fundamentals_of_language.Pointers()
 
-	println("\n14. Internal Arrays")
+	fmt.Println("\n14. Internal Arrays")
 	fundamentals_of_language.InternalArrays()
+
+	fmt.Println("\n15. Maps")
+	fundamentals_of_language.Maps()
 }
