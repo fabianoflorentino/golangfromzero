12ff4282cf8dbfb5f762c016c62dd9a7579b822f
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2025-12-30 20:23:58 -0300

    feat: configure user routes

--- src/router/router.go
+++ src/router/router.go
@@ -3,11 +3,14 @@ package router
 import (
 	"fmt"
 
+	"github.com/fabianoflorentino/golangfromzero/src/routes"
 	"github.com/gorilla/mux"
 )
 
 // NewRouter creates and returns a new Gorilla Mux router instance.
 func NewRouter() *mux.Router {
 	fmt.Println("server up and running on port 6000")
-	return mux.NewRouter()
+	r := mux.NewRouter()
+
+	return routes.Configure(r)
 }
--- src/routes/routes.go
+++ src/routes/routes.go
@@ -1,6 +1,10 @@
 package routes
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 // Route defines the structure for an API route
 type Route struct {
@@ -8,4 +12,15 @@ type Route struct {
 	Method        string
 	Function      func(http.ResponseWriter, *http.Request)
 	Authenticated bool
+}
+
+// Configure sets up the routes in the provided router
+func Configure(r *mux.Router) *mux.Router {
+	u := UsersRoutes
+
+	for _, route := range u {
+		r.HandleFunc(route.URI, route.Function).Methods(route.Method)
+	}
+
+	return r
 }
