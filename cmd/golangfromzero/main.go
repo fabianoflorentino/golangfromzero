b4a9a2b2a789d39b6c34f686255acb9fbf8d3990
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2025-12-30 19:03:39 -0300

    feat: create a new instance http server with gorilla mux

--- cmd/golangfromzero/main.go
+++ cmd/golangfromzero/main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/fabianoflorentino/golangfromzero/src/router"
 )
 
 func main() {
-	fmt.Println("Learn Golang from Zero!")
+	r := router.NewRouter()
+
+	if err := http.ListenAndServe(":6000", r); err != nil {
+		log.Fatal(err)
+	}
 }
