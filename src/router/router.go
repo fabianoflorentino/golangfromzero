7f3d775e5f5765b6ea0fe752a8ae67cb705a2a9f
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2025-12-31 10:17:40 -0300

    chore: simplified the router

--- src/router/router.go
+++ src/router/router.go
@@ -1,16 +1,12 @@
 package router
 
 import (
-	"fmt"
-
 	"github.com/fabianoflorentino/golangfromzero/src/routes"
 	"github.com/gorilla/mux"
 )
 
 // NewRouter creates and returns a new Gorilla Mux router instance.
 func NewRouter() *mux.Router {
-	fmt.Println("server up and running on port 6000")
 	r := mux.NewRouter()
-
 	return routes.Configure(r)
 }
