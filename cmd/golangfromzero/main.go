7ca10c873d422e5283858b60735216b5fd0bf565
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2025-12-31 10:18:40 -0300

    feat: configure loadenv function

--- cmd/golangfromzero/main.go
+++ cmd/golangfromzero/main.go
@@ -1,16 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
+	"github.com/fabianoflorentino/golangfromzero/config"
 	"github.com/fabianoflorentino/golangfromzero/src/router"
 )
 
+func init() {
+	config.LoadEnv()
+}
+
 func main() {
 	r := router.NewRouter()
+	app_port := fmt.Sprintf(":%d", config.Port)
 
-	if err := http.ListenAndServe(":6000", r); err != nil {
+	if err := http.ListenAndServe(app_port, r); err != nil {
 		log.Fatal(err)
 	}
 }
