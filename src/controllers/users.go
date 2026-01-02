0fd6fe3c576430d2db848f8c93895365a8f420df
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2026-01-01 21:50:50 -0300

    feat: updata user controller

--- src/controllers/users.go
+++ src/controllers/users.go
@@ -1,10 +1,48 @@
 package controllers
 
-import "net/http"
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+
+	"github.com/fabianoflorentino/golangfromzero/database"
+	"github.com/fabianoflorentino/golangfromzero/repository"
+	"github.com/fabianoflorentino/golangfromzero/src/models"
+)
 
 // Create handles the creation of a new user
 func Create(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("User created"))
+	requestBody, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "failed to read request body", http.StatusBadRequest)
+		return
+	}
+
+	var u models.User
+	if err := json.Unmarshal(requestBody, &u); err != nil {
+		http.Error(w, "failed, invalid JSON", http.StatusBadRequest)
+		return
+	}
+
+	db, err := database.Connect()
+	if err != nil {
+		http.Error(w, "database unavailable", http.StatusInternalServerError)
+		return
+	}
+	defer db.Close()
+
+	repository := repository.NewUsersRepository(db)
+	id, err := repository.Create(u)
+	if err != nil {
+		http.Error(w, "failed to create user", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(map[string]any{
+		"id":      id,
+		"message": "user created",
+	})
 }
 
 // GetAll retrieves all users
