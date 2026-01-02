8328359811bb0cfac6035a76e44d755df738193f
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2026-01-01 21:51:15 -0300

    chore: improved the database connection

--- cmd/golangfromzero/main.go
+++ cmd/golangfromzero/main.go
@@ -1,23 +1,34 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/fabianoflorentino/golangfromzero/config"
+	"github.com/fabianoflorentino/golangfromzero/database"
 	"github.com/fabianoflorentino/golangfromzero/src/router"
+
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-func init() {
-	config.LoadEnv()
-}
-
 func main() {
-	r := router.NewRouter()
-	app_port := fmt.Sprintf(":%d", config.Port)
+	config.LoadEnv()
 
-	if err := http.ListenAndServe(app_port, r); err != nil {
+	ctx := context.Background()
+	dsn := database.ConnectionString
+
+	db, err := pgxpool.New(ctx, dsn)
+	if err != nil {
+		log.Fatalf("Unable to connect to database: %v\n", err)
+	}
+	defer db.Close()
+
+	r := router.NewRouter()
+	appPort := fmt.Sprintf(":%d", config.Port)
+
+	if err := http.ListenAndServe(appPort, r); err != nil {
 		log.Fatal(err)
 	}
 }
