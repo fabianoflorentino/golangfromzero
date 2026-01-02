a7881f34ddd729adb0e2a9ebe2b090218902547b
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2026-01-02 14:50:04 -0300

    feat: added method of validation fields and remove leading and trailing white space

--- src/models/user.go
+++ src/models/user.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"strings"
 	"time"
 
 	"github.com/google/uuid"
@@ -13,4 +14,36 @@ type User struct {
 	Email     string    `json:"email,omitempty"`
 	Password  string    `json:"password,omitempty"`
 	CreatedAt time.Time `json:"created_at"`
+}
+
+// Validate check if any field is blank, and with all leading and trailing white space removed
+func (u *User) Validate() error {
+	if err := u.isBlank(); err != nil {
+		return err
+	}
+
+	u.trimSpace()
+
+	return nil
+}
+
+func (u *User) isBlank() error {
+	if u.Name == "" {
+		return ErrNameBlank
+	}
+
+	if u.Email == "" {
+		return ErrEmailBlank
+	}
+
+	if u.Password == "" {
+		return ErrPasswordBlank
+	}
+
+	return nil
+}
+
+func (u *User) trimSpace() {
+	strings.TrimSpace(u.Name)
+	strings.TrimSpace(u.Email)
 }
