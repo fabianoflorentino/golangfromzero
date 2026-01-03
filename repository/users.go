ab1ba13997537414bc369897240e96c9b80490c4
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2026-01-03 13:35:20 -0300

    feat: added method search by name and id

--- repository/users.go
+++ repository/users.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/fabianoflorentino/golangfromzero/src/models"
 	"github.com/google/uuid"
@@ -27,4 +28,44 @@ func (r UserRepository) Create(user models.User) (uuid.UUID, error) {
 	}
 
 	return id, nil
+}
+
+func (r UserRepository) SearchByName(name string) ([]models.User, error) {
+	query := `SELECT id, name, email, created_at from users WHERE name LIKE $1`
+
+	var users []models.User
+	ctx := context.Background()
+
+	name = fmt.Sprintf("%%%s%%", name)
+
+	rows, err := r.db.Query(ctx, query, name)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var user models.User
+
+		if err := rows.Scan(&user.ID, &user.Name, &user.Email, &user.CreatedAt); err != nil {
+			return nil, err
+		}
+
+		users = append(users, user)
+	}
+
+	return users, nil
+}
+
+func (r UserRepository) SearchByID(id uuid.UUID) (*models.User, error) {
+	query := `SELECT id, name, email, created_at FROM users WHERE id = $1`
+
+	var user models.User
+	ctx := context.Background()
+
+	if err := r.db.QueryRow(ctx, query, id).Scan(&user.ID, &user.Name, &user.Email, &user.CreatedAt); err != nil {
+		return nil, err
+	}
+
+	return &user, nil
 }
