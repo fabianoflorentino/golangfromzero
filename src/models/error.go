64c53ec14cc9790053da9e48f3d4167d3d394e15
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2026-01-05 12:05:34 -0300

    feat: msg error for user exists

--- src/models/error.go
+++ src/models/error.go
@@ -3,7 +3,9 @@ package models
 import "errors"
 
 var (
-	ErrNameBlank     error = errors.New("name can not be blank")
-	ErrEmailBlank    error = errors.New("email can not be blank")
-	ErrPasswordBlank error = errors.New("password can not be blank")
+	ErrNameBlank          error = errors.New("name can not be blank")
+	ErrEmailBlank         error = errors.New("email can not be blank")
+	ErrPasswordBlank      error = errors.New("password can not be blank")
+	ErrInvalidEmailFormat error = errors.New("invalid format")
+	ErrEmailAlreadyExist  error = errors.New("email already used")
 )
