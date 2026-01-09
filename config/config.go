c085c4ac2a37f071a005bbd4bd0dfab954aa25ff
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2026-01-09 09:22:05 -0300

    feat: improved config with new AppPort function

--- config/config.go
+++ config/config.go
@@ -12,15 +12,19 @@ var Port int
 var PathEnv string = "/golangfromzero/config"
 
 func LoadEnv() {
-	var err error
 
 	if err := godotenv.Load(PathEnv + "/.env"); err != nil {
 		log.Fatalf("Error loading .env file: %v", err)
 	}
+}
 
+func AppPort() int {
 	serverPort := os.Getenv("SERVER_PORT")
-	Port, err = strconv.Atoi(serverPort)
+
+	Port, err := strconv.Atoi(serverPort)
 	if err != nil {
-		Port = 6000
+		return 6000
 	}
+
+	return Port
 }
