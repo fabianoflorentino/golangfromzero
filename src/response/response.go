690278737f429382839fe42e7969b9195a8b112b
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2026-01-08 23:46:01 -0300

    doc: added for functions

--- src/response/response.go
+++ src/response/response.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 )
 
+// Err write a error message in JSON with status code and error message.
 func Err(w http.ResponseWriter, statusCode int, err error) {
 	JSON(w, statusCode, struct {
 		Err string `json:"error"`
@@ -14,6 +15,7 @@ func Err(w http.ResponseWriter, statusCode int, err error) {
 	})
 }
 
+// JSON set headers and write the status code, encode the data response.
 func JSON(w http.ResponseWriter, statusCode int, dataResponse any) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(statusCode)
