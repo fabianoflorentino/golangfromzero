e65a15a610a51db691d06d074d4e01173851cc05
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2026-01-10 00:54:02 -0300

    feat: improve user controller with injection configuration

--- src/router/router.go
+++ src/router/router.go
@@ -1,12 +1,17 @@
 package router
 
 import (
+	"github.com/fabianoflorentino/golangfromzero/src/controllers"
+	"github.com/fabianoflorentino/golangfromzero/src/helper"
 	"github.com/fabianoflorentino/golangfromzero/src/routes"
 	"github.com/gorilla/mux"
 )
 
 // NewRouter creates and returns a new Gorilla Mux router instance.
 func NewRouter() *mux.Router {
+	cfg := helper.ConfigTimeout
+	userController := controllers.NewUserController(cfg)
+
 	r := mux.NewRouter()
-	return routes.Configure(r)
+	return routes.Configure(r, userController)
 }
