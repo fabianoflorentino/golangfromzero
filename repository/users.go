b426719505a4ec200bbcd508222bdcf84518ab02
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2026-01-10 00:50:13 -0300

    chore: improved context use on functions

--- repository/users.go
+++ repository/users.go
@@ -19,9 +19,8 @@ func NewUsersRepository(db *pgxpool.Pool) *UserRepository {
 	return &UserRepository{db}
 }
 
-func (r UserRepository) Create(user models.User) (uuid.UUID, error) {
+func (r UserRepository) Create(ctx context.Context, user models.User) (uuid.UUID, error) {
 	query := `INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING id`
-	ctx := context.Background()
 
 	var id uuid.UUID
 
@@ -36,11 +35,10 @@ func (r UserRepository) Create(user models.User) (uuid.UUID, error) {
 	return id, nil
 }
 
-func (r UserRepository) SearchByName(name string) ([]models.User, error) {
+func (r UserRepository) SearchByName(ctx context.Context, name string) ([]models.User, error) {
 	query := `SELECT id, name, email, created_at from users WHERE name LIKE $1`
 
 	var users []models.User
-	ctx := context.Background()
 
 	name = fmt.Sprintf("%%%s%%", name)
 
@@ -63,11 +61,10 @@ func (r UserRepository) SearchByName(name string) ([]models.User, error) {
 	return users, nil
 }
 
-func (r UserRepository) SearchByID(id uuid.UUID) (*models.User, error) {
+func (r UserRepository) SearchByID(ctx context.Context, id uuid.UUID) (*models.User, error) {
 	query := `SELECT id, name, email, created_at FROM users WHERE id = $1`
 
 	var user models.User
-	ctx := context.Background()
 
 	if err := r.db.QueryRow(ctx, query, id).Scan(&user.ID, &user.Name, &user.Email, &user.CreatedAt); err != nil {
 		return nil, err
@@ -76,10 +73,8 @@ func (r UserRepository) SearchByID(id uuid.UUID) (*models.User, error) {
 	return &user, nil
 }
 
-func (r UserRepository) Update(id uuid.UUID, user models.User) error {
+func (r UserRepository) Update(ctx context.Context, id uuid.UUID, user models.User) error {
 	query := `UPDATE users SET name = $1, email = $2 WHERE id = $3`
-
-	ctx := context.Background()
 
 	if _, err := r.db.Exec(ctx, query, user.Name, user.Email, id); err != nil {
 		return err
@@ -88,10 +83,8 @@ func (r UserRepository) Update(id uuid.UUID, user models.User) error {
 	return nil
 }
 
-func (r UserRepository) Delete(id uuid.UUID) error {
+func (r UserRepository) Delete(ctx context.Context, id uuid.UUID) error {
 	query := `DELETE FROM users WHERE id = $1`
-
-	ctx := context.Background()
 
 	if _, err := r.db.Exec(ctx, query, id); err != nil {
 		return err
