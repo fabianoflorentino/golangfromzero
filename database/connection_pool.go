a572d6cd466cc50d450da378e495cee137110e43
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2026-01-11 13:01:27 -0300

    feat: improved the database connection with connection pool advanced configuration

---
+++ database/connection_pool.go
@@ -0,0 +1,100 @@
+package database
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type ConnectionPoolConfig struct {
+	Host                  string
+	Port                  string
+	User                  string
+	Password              string
+	Database              string
+	SSLmode               string
+	MaxConnections        int32
+	MinConnections        int32
+	MaxConnectionLifetime time.Duration
+	MaxConnectionIdletime time.Duration
+	HealthcheckPeriod     time.Duration
+}
+
+func LoadConnectionPoolConfig() ConnectionPoolConfig {
+	return ConnectionPoolConfig{
+		Host:                  getEnv("DB_HOST", "localhost"),
+		Port:                  getEnv("DB_PORT", "5432"),
+		User:                  getEnv("DB_USER", "postgres"),
+		Password:              getEnv("DB_PASSWORD", "postgres"),
+		Database:              getEnv("DB_NAME", "exampledb"),
+		SSLmode:               getEnv("DB_SSLMODE", "disable"),
+		MaxConnections:        getEnvAsInt32("DB_MAX_CONNECTIONS", 25),
+		MinConnections:        getEnvAsInt32("DB_MIN_CONNEXTIONS", 5),
+		MaxConnectionLifetime: getEnvAsDuration("DB_MAX_CONNECTION_LIFETIME", 5*time.Minute),
+		MaxConnectionIdletime: getEnvAsDuration("DB_MAX_CONNECTION_IDLETIME", 1*time.Minute),
+		HealthcheckPeriod:     getEnvAsDuration("DB_HEALTHCHECK_PERIOD", 1*time.Minute),
+	}
+}
+
+func NewConnectionPool(ctx context.Context, cfg ConnectionPoolConfig) (*pgxpool.Pool, error) {
+	connectionString := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.Database, cfg.SSLmode)
+
+	connectionPoolConfig, err := pgxpool.ParseConfig(connectionString)
+	if err != nil {
+		return nil, fmt.Errorf("unable to parse database config: %w", err)
+	}
+
+	connectionPoolConfig.MaxConns = cfg.MaxConnections
+	connectionPoolConfig.MinConns = cfg.MinConnections
+	connectionPoolConfig.MaxConnLifetime = cfg.MaxConnectionLifetime
+	connectionPoolConfig.MaxConnIdleTime = cfg.MaxConnectionIdletime
+
+	connectionPool, err := pgxpool.NewWithConfig(ctx, connectionPoolConfig)
+	if err != nil {
+		return nil, fmt.Errorf("unable to create database pool: %w", err)
+	}
+
+	if err := connectionPool.Ping(ctx); err != nil {
+		connectionPool.Close()
+		return nil, fmt.Errorf("unable to ping database: %w", err)
+	}
+
+	log.Printf("database connection pool created successfully (max: %d, min: %d)", cfg.MaxConnections, cfg.MinConnections)
+
+	return connectionPool, nil
+}
+
+func getEnv(env, defaultValue string) string {
+	if value := os.Getenv(env); value != "" {
+		return value
+	}
+
+	return defaultValue
+}
+
+func getEnvAsInt32(env string, defaultValue int32) int32 {
+	if value := os.Getenv(env); value != "" {
+		var intValue int32
+
+		fmt.Scanf(value, "%d", &intValue)
+		return intValue
+	}
+
+	return defaultValue
+}
+
+func getEnvAsDuration(env string, defaultValue time.Duration) time.Duration {
+	if value := os.Getenv(env); value != "" {
+		duration, err := time.ParseDuration(value)
+		if err == nil {
+			return duration
+		}
+	}
+
+	return defaultValue
+}
