08ec6d88fd7f6d8ef37b1cc4f8f8ffe98fd37d2d
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2026-01-12 14:42:56 -0300

    feat: create a start function to http server

---
+++ internal/server/http.go
@@ -0,0 +1,42 @@
+package server
+
+import (
+	"context"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+func Start(ctx context.Context, router *mux.Router) error {
+	srv := &http.Server{
+		Addr:         ":8080",
+		Handler:      router,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  120 * time.Second,
+	}
+
+	go func() {
+		slog.Info("server starting", "address", srv.Addr)
+
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("server failed to start: %v", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("server forced to shutdown: %s", err)
+	}
+
+	return nil
+}
