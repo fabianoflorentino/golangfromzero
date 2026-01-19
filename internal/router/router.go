64a5268b1035aa0d93583e4604018da75f9f2255
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2026-01-19 12:41:57 -0300

    feat: update NewRouter to include logging and user repository

--- internal/router/router.go
+++ internal/router/router.go
@@ -1,15 +1,19 @@
 package router
 
 import (
+	"log/slog"
+
 	"github.com/fabianoflorentino/golangfromzero/internal/controllers"
 	"github.com/fabianoflorentino/golangfromzero/internal/routes"
+	"github.com/fabianoflorentino/golangfromzero/repository"
 	"github.com/gorilla/mux"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 // NewRouter creates and returns a new Gorilla Mux router instance.
-func NewRouter(db *pgxpool.Pool) *mux.Router {
-	userController := controllers.NewUserController(db)
+func NewRouter(db *pgxpool.Pool, log *slog.Logger) *mux.Router {
+	userRepo := repository.NewUserRepository(db, log)
+	userController := controllers.NewUserController(db, userRepo, log)
 
 	r := mux.NewRouter()
 	return routes.Configure(r, userController)
