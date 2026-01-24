754d9586a5aee053879ccd77e629f198a694c224
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2026-01-24 12:40:01 -0300

    feat: implement Run function for application execution and server management

---
+++ internal/app/app.go
@@ -0,0 +1,34 @@
+package app
+
+import (
+	"context"
+	"log/slog"
+
+	"github.com/fabianoflorentino/golangfromzero/internal/router"
+	"github.com/fabianoflorentino/golangfromzero/internal/server"
+)
+
+func Run() error {
+	ctx := context.Background()
+	logger := NewLogger()
+
+	pool, err := NewDatabasePool(ctx, logger)
+	if err != nil {
+		slog.ErrorContext(ctx, "database init failed", "error", err)
+		return err
+	}
+	defer pool.Close()
+
+	slog.InfoContext(ctx, "database connection pool initialized")
+
+	r := router.NewRouter(pool, logger)
+
+	if err := server.Start(ctx, r); err != nil {
+		slog.ErrorContext(ctx, "http server start failed", "error", err)
+		return err
+	}
+
+	slog.InfoContext(ctx, "server exited gracefully")
+
+	return nil
+}
