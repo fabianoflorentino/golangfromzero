37fa28c51d239eaafe4a830579a9ca79006a21e3
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2026-01-24 12:40:17 -0300

    refactor: simplify main function by removing unused variables and logging setup

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,44 +1,17 @@
 package main
 
 import (
-	"context"
 	"log"
-	"log/slog"
-	"os"
 
-	"github.com/fabianoflorentino/golangfromzero/database"
-	"github.com/fabianoflorentino/golangfromzero/internal/router"
-	"github.com/fabianoflorentino/golangfromzero/internal/server"
+	"github.com/fabianoflorentino/golangfromzero/internal/app"
 	"github.com/joho/godotenv"
 )
 
 func main() {
 
-	if err := godotenv.Load(); err != nil {
-		log.Fatalf("error load environment variables:  %v", err.Error())
+	_ = godotenv.Load()
+
+	if err := app.Run(); err != nil {
+		log.Fatalf("application failed to start: %v", err)
 	}
-
-	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
-	slog.SetDefault(logger)
-
-	databaseConfig := database.LoadConnectionPoolConfig()
-
-	ctx := context.Background()
-
-	pool, err := database.NewConnectionPool(ctx, databaseConfig)
-	if err != nil {
-		log.Fatalf("falied to create database pool: %s", err)
-	}
-	defer pool.Close()
-
-	slog.Info("database connection pool initialized successfully")
-
-	r := router.NewRouter(pool, logger)
-
-	if err := server.Start(ctx, r); err != nil {
-		log.Fatalf("failed to start http server: %s", err)
-	}
-
-	pool.Close()
-	slog.Info("server exited gracefully")
 }
