d048dd79eba8e8341ee06b66d3bc849fd24ec979
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2026-01-24 12:39:55 -0300

    feat: implement NewDatabasePool function for database connection management

---
+++ internal/app/database.go
@@ -0,0 +1,26 @@
+package app
+
+import (
+	"context"
+	"log"
+	"log/slog"
+
+	"github.com/fabianoflorentino/golangfromzero/database"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func NewDatabasePool(ctx context.Context, logger *slog.Logger) (*pgxpool.Pool, error) {
+	databaseConfig, err := database.LoadConnectionPoolConfig()
+	if err != nil {
+		log.Fatalf("failed to load database config: %s", err)
+	}
+
+	db, err := database.NewConnectionPool(ctx, databaseConfig)
+	if err != nil {
+		log.Fatalf("failed to create database pool: %s", err)
+	}
+
+	logger.InfoContext(ctx, "database connection established")
+
+	return db, nil
+}
